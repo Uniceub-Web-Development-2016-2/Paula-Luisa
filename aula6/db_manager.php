b78bb71431aa3b201df8ef303d339403f2e964be
root <you@example.com>
2016-09-27 18:53:07 -0300

    Banco de dados + get

---
+++ aula6/db_manager.php
@@ -0,0 +1,26 @@
+<?php
+
+class DBConnector extends PDO {
+   
+    private $engine;
+    private $host;
+    private $database;
+    private $user;
+    private $pass;
+    private $connector;	
+   
+    public function __construct(){
+        $this->engine = 'mysql';
+        $this->host = 'localhost';
+        $this->database = 'aula';
+        $this->user = 'root';
+        $this->pass = '';
+        $dns = $this->engine.':dbname='.$this->database.";host=".$this->host;
+        parent::__construct( $dns, $this->user, $this->pass );
+    }	
+		
+}
+
+
+//var_dump($result->fetchAll());
+
