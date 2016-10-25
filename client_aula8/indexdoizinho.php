67d563fe3a5a8b073aa8fb35a94bd4cd8aa57aa7
root <you@example.com>
2016-10-25 20:26:09 -0200

    Usando o Client

---
+++ client_aula8/indexdoizinho.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<body>
+
+<form action="requestinsert.php" method="post">
+  <input type="text" name="first_name" value="Insira um Primeiro Nome">
+  <input type="text" name="last_name" value="Insira um Segundo Nome.">
+  <input type="email" name="email" value="Insira um email">
+  <input type="password" name="password" value="Insira uma senha">
+
+  <input type="submit" value="Submit">
+
+
+</form>
+
+</body>
+
+
+
+</html>
+
